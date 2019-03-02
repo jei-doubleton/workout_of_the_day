f1e448b48bb6dee92c492e99608e6a28689b4282
J. Singleton <jennifer.singleton@gmail.com>
2019-03-02 22:50:44 +0000

    more refinements to cli flow

--- lib/workout_of_the_day/cli.rb
+++ lib/workout_of_the_day/cli.rb
@@ -1,11 +1,10 @@
 class WorkoutOfTheDay::CLI
-  attr_reader :input
+  attr_reader :workout_input, :more_workouts_input
 
   def call
-    while @input != "Exit"
       list_workouts
       workout_menu
-    end
+      goodbye
   end
 
   def list_workouts
@@ -18,19 +17,48 @@ class WorkoutOfTheDay::CLI
     DOC
   end
 
+  def workout_menu_instructions
+    puts "Enter the number of the workout you'd like details on or enter 'exit' to exit the program."
+  end
+
+  def invalid_input
+    puts "Invalid input."
+    workout_menu_instructions
+  end
+
   def workout_menu
-    puts "Enter the number of the workout you'd like details on or enter 'Exit' to exit the program."
-    @input = gets.strip
-    case @input
-    when "1"
-      puts "Crossfit workout"
-    when "2"
-      puts "Rowing workout"
-    when "3"
-      puts "military workout"
-    when "4"
-      puts "bodyweight workout"
+    workout_menu_instructions
+    @workout_input = gets.strip.downcase
+    while @workout_input != "exit"
+      if @workout_input == "1"
+        puts "Crossfit workout"
+      elsif @workout_input == "2"
+        puts "Rowing workout"
+      elsif @workout_input == "3"
+        puts "military workout"
+      elsif @workout_input == "4"
+        puts "bodyweight workout"
+      elsif @workout_input != "exit"
+        invalid_input
+      end
+      more_workouts?
     end
+  end
+
+  def more_workouts?
+    puts "Would you like to see another workout? (y/n)"
+    @more_workouts_input = gets.strip.downcase
+    if @more_workouts_input == "y"
+      workout_menu
+    elsif @more_workouts_input == "n"
+      @workout_input = "exit"
+    else
+      invalid_input
+    end
+  end
+
+  def goodbye
+    puts "See you tomorrow for your next workout!"
   end
 
 end
