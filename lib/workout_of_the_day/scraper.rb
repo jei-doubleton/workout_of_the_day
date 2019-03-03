17c6905fdaf6bcc708abc8b68fc1db3837fb19f7
J. Singleton <jennifer.singleton@gmail.com>
2019-03-03 00:34:33 +0000

    add scrape_crossfit to scraper class

--- lib/workout_of_the_day/scraper.rb
+++ lib/workout_of_the_day/scraper.rb
@@ -1,27 +1,33 @@
 class WorkoutOfTheDay::Scraper
 
-  def scrape_workouts
+  def self.scrape_workouts
     scrape_crossfit
     scrape_rowing
     scrape_military
     scrape_bodyweight
   end
 
-  def scrape_crossfit
-    #go to crossfit.com
-    #extract properties
-    #create Workout.new
+  def self.scrape_crossfit
+    doc = Nokogiri::HTML(open("https://www.crossfit.com/workout/"))
+
+    name = doc.search("h3 a").first.text
+    url = doc.search("h3 a").first.attr("href")
+
+    workout = WorkoutOfTheDay::Workout.new
+    workout.name = name
+    workout.url = "https://www.crossfit.com/#{url}"
+    workout
   end
 
-  def scrape_rowing
+  def self.scrape_rowing
 
   end
 
-  def scrape_military
+  def self.scrape_military
 
   end
 
-  def scrape_bodyweight
+  def self.scrape_bodyweight
 
   end
 
--- lib/workout_of_the_day/workout.rb
+++ lib/workout_of_the_day/workout.rb
@@ -3,20 +3,12 @@ class WorkoutOfTheDay::Workout
 
   @@all = []
 
+  def initialize
+    @@all << self
+  end
+
   def self.all
-    workout_1 = self.new
-    workout_1.name = "Crossfit: Saturday 190302"
-
-    workout_2 = self.new
-    workout_2.name = "Rowing: 5000m with rate changes"
-
-    workout_3 = self.new
-    workout_3.name = "Military: SOF WOD MAR 1"
-
-    workout_4 = self.new
-    workout_4.name = "No Equipment, No Running Bodyweight WODs"
-
-    [workout_1, workout_2, workout_3, workout_4]
+    @@all
   end
 
 
