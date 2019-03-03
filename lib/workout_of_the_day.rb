3914618a3930d1d20091e4f1e54e99d402264ed1
J. Singleton <jennifer.singleton@gmail.com>
2019-03-03 00:13:58 +0000

    add pry and nokogiri dependencies

--- lib/workout_of_the_day.rb
+++ lib/workout_of_the_day.rb
@@ -1,3 +1,7 @@
+require "nokogiri"
+require "pry"
+
 require_relative "./workout_of_the_day/version"
 require_relative "./workout_of_the_day/cli"
 require_relative "./workout_of_the_day/workout"
+require_relative "./workout_of_the_day/scraper"
